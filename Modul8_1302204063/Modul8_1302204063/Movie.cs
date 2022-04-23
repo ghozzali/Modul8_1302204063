42d6ffcd02a02d430c1cfa9266d60e915b087d98
ghozzali <ghozal.perdana02@gmail.com>
2022-04-23 16:56:31 +0700

    last movie commit

---
+++ Modul8_1302204063/Modul8_1302204063/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace Modul8_1302204063
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+        public Movie(string title, string director, List<string> stars, string description)
+        {
+            Title = title;
+            Director = director;
+            Stars = stars;
+            Description = description;
+        }
+    }
+}
